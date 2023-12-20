613032a230c4b101d82b1bb03d0ffd0eb5926550
Kurnia Ramadhan Putra <kurniaramadhan@itenas.ac.id>
2023-12-20 08:36:22 +0700

    Initial commit

---
+++ LatihanGit/src/main/java/com/itenas/latihangit/BangunDatar.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itenas.latihangit;
+
+/**
+ *
+ * @author Kurnia R. Putra
+ */
+abstract class BangunDatar {
+    private String nama;
+    private int jumlahSudut;
+
+    public BangunDatar() {
+    }
+
+    public BangunDatar(String nama, int jumlahSudut) {
+        this.nama = nama;
+        this.jumlahSudut = jumlahSudut;
+    }
+    
+    public abstract double hitungLuas();
+    public abstract double hitungKeliling();
+    
+    
+}
